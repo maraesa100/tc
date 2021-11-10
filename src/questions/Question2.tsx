88e55798b14084cf055eee52d0707592f245b57d
maraesa100 <siobhanmckenzie90@gmail.com>
2021-11-10 19:20:17 +0000

    feat(technical-test): component structure

--- src/questions/Question2.tsx
+++ src/questions/Question2.tsx
@@ -7,8 +7,24 @@
   Bonus points: make the app fun to use (like a game), especially if the user finds a word with many anagrams.
 */
 
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { getAllAnagramData, anagramObj } from '../app/features/anagram/anagramSlice'
+
 const Question2 = () => {
-  return <h1>Build your app here</h1>;
+  const dispatch = useDispatch()
+  const anagrams = useSelector(anagramObj)
+
+  useEffect(() => {
+    dispatch(getAllAnagramData())
+  }, [dispatch])
+
+  return <h1>Build your app here...
+    
+    {/* {anagrams.data.map(item => <p>{item}</p>)} */}
+
+  </h1>;
 };
 
 export default Question2;
