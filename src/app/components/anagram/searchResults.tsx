2b384552c91dc0722e0c6b612b7c7e3ad21915da
maraesa100 <siobhanmckenzie90@gmail.com>
2021-11-12 12:12:40 +0000

    feat(tech-test): functionality completed

--- src/app/components/anagram/searchResults.tsx
+++ src/app/components/anagram/searchResults.tsx
@@ -12,15 +12,14 @@ export const SearchResults: React.FC<searchResultsProps> = ({
 }: searchResultsProps) => {
   return (
     <div>
-      <h2>{title}</h2>
+      <p className='matched-word'>{title}</p>
         <p key={index}></p>
       {results &&
         results.map((title: any) => {
-          return <p className='title'>{title.map((i: any) => {
+          return <div className='word-result'>{title.map((i: any) => {
             return (<p>{i}</p>)
-            })}</p>
-          })}
-        
+            })}</div>
+          })} 
 </div>
   )
 }
